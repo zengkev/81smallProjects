9f132d1538545ed1397224355d847097922b58b5
jzeng99 <jzeng99@gatech.edu>
2025-06-03 11:22:25 -0400

    Fixed: Need more work on Algo

--- bouncingDVDLogo.py
+++ bouncingDVDLogo.py
@@ -46,15 +46,91 @@ def main():
                      DIR: random.choice(directions)})
         if logos[-1][X] % 2 == 1:
             # making sure X is even so it will hit the corner
-            logos[-1][X] -=1
+            logos[-1][X] -= 1
 
     cornerBounces = 0 # Count times a logo hits the corner
     while True:
         for logo in logos:
+            # earse the logo current location to check new coordination
+            bext.goto(logo[X], logo[Y])
+            print('   ', end='')
+
+            originalDirection = logo[DIR]
+
+            # See if the logo bounces off the corner
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = downRight
+                cornerBounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = upRIGHT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = downLeft
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = upLeft
+                cornerBounces += 1
+
+            # Check if the logo bounces off the left edge
+            elif logo[X] == 0 and logo[DIR] == upLeft:
+                logo[DIR] = upRIGHT
+            elif logo[X] == 0 and logo[DIR] == downLeft:
+                logo[DIR] = downRight
+
+            # Check if the logo bounces off the right edge
+            elif logo[X] == WIDTH - 3 and logo[DIR] == upRIGHT:
+                logo[DIR] = upLeft
+            elif logo[X] == WIDTH - 3 and logo[DIR] == downRight:
+                logo[DIR] = downLeft
             
+            # Check if the logo bounces off the top edge
+            elif logo[Y] == 0 and logo[DIR] == upLeft:
+                logo[DIR] = upRIGHT
+            elif logo[Y] == 0 and logo[DIR] == downLeft:
+                logo[DIR] = downRight
 
+            # Check if the logo bounces off the bottom edge
 
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == downLeft:
+                logo[DIR] = upLeft
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == downRight:
+                logo[DIR] = upRIGHT
 
+            if logo[DIR] != originalDirection:
+                # Change the color when bounces
+                logo[COLOR] = random.choice(colors)
+
+            # Moving the logos
+            if logo[DIR] == upRIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == upLeft:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == downRight:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == downLeft:
+                logo[X] -= 2
+                logo[Y] += 1
+
+            # Clamp positions to avoid negative y coordinates
+            logo[X] = max(0, min(WIDTH - 3, logo[X]))
+            logo[Y] = max(0, min(HEIGHT - 1, logo[Y]))
+
+            # Display number of corners bounced
+            bext.goto(5, 0)
+            bext.fg('white')
+            print('Corner Bounced: ', cornerBounces, end='')
+
+            for logo in logos:
+                bext.goto(logo[X], logo[Y])
+                bext.fg(logo[COLOR])
+                print('DVD', end='')
+            
+        bext.goto(0, 0)
+        sys.stdout.flush()
+        time.sleep(pauseAmount)
 
 # Start the program but CTRL C to stop
 
