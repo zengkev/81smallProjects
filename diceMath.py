bc2311cc4e9670c57721f3d70d5147593271a907
jzeng99 <jzeng99@gatech.edu>
2025-06-14 22:03:48 -0400

    Add up the Dice ASAP

---
+++ diceMath.py
@@ -0,0 +1,37 @@
+'''Dice Math: Add up the Dice ASAP'''
+
+print('By Jing Wei Zeng')
+print('Linkedin: https://www.linkedin.com/in/zengkev/')
+print()
+print()
+
+import random, time
+
+# Set up the constants
+
+
+# Set up the durations
+
+# Draw Dice Faces
+
+# How to play the game message
+
+input('Press Enter to begin...')
+
+# Keep track of how many answers were correct and incorrect
+correct = 0
+incorrect = 0
+startTime = time.time()
+
+    # start the timer and main loop
+
+    # draw the dice on the canvas
+
+
+    # Display the canvas on the screen
+
+    # Let the player enter their answers
+
+
+# Display the final scores
+
